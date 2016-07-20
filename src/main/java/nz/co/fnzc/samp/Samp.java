faa16629ad28b9226f647d34393a528103e78076
Alex Cumming <alex.cumming@fnzc.co.nz>
2016-07-20 15:43:40 +1200

    Fix bug with map in Samp.format

--- src/main/java/nz/co/fnzc/samp/Samp.java
+++ src/main/java/nz/co/fnzc/samp/Samp.java
@@ -129,7 +129,8 @@ public class Samp {
     /** Format a SAMP message as bytes from the given parameters */
     public static byte[] format(String kind, Optional<String> status, String action, Map<String, String> headers, Optional<byte[]> body) {
         // TODO should use byte buffer here instead of string builder...
-        headers.putIfAbsent(Samp.Date, formatDate(new Date()));
+        Map<String, String> sortedHeaders = new TreeMap<>(headers);
+        sortedHeaders.putIfAbsent(Samp.Date, formatDate(new Date()));
         final StringBuilder sb = new StringBuilder("SAMP/1.0 ");
         sb.append(kind);
         if (status.isPresent()) {
@@ -139,7 +140,7 @@ public class Samp {
         sb.append(" ");
         sb.append(action);
         sb.append("\n");
-        new TreeMap<>(headers).forEach((k, v) -> {
+        sortedHeaders.forEach((k, v) -> {
                 sb.append(k);
                 sb.append(": ");
                 sb.append(v);
